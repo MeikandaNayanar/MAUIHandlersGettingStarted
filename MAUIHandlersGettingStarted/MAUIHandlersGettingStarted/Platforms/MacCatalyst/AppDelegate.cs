8b4408c2fb7e16c9ec9a1613930db5b3d9214733
MeikandaNayanarI <meikandai@syncfusion.com>
2021-08-19 14:32:32 +0530

    Add project files.

---
+++ MAUIHandlersGettingStarted/MAUIHandlersGettingStarted/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+using Microsoft.Maui;
+
+namespace MAUIHandlersGettingStarted
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate<Startup>
+	{
+	}
+}
